9d7a26ad00eda0aca7ee7815bb62bb3afcd30ef8
Ademola <demolafadumo@gmail.com>
2022-05-31 23:17:58 +0100

    reset password view implemented

---
+++ lib/src/ui/views/auth/reset_password/reset_password_view.dart
@@ -0,0 +1,80 @@
+import 'package:apex_network_take_home_project/src/ui/views/auth/base_auth/base_auth_view.dart';
+import 'package:apex_network_take_home_project/src/ui/views/auth/reset_password/reset_password_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../core/constants/colors.dart';
+import '../../../core/constants/component_sizes.dart';
+import '../../../core/constants/text_styles.dart';
+import '../../../shared/stateless/gap.dart';
+import '../../../shared/stateless/text_field.dart';
+import '../../../shared/stateless/visibility_toggle.dart';
+
+class ResetPasswordView extends ConsumerWidget {
+  const ResetPasswordView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewState = ref.watch(resetPasswordViewModel);
+    return Scaffold(
+      backgroundColor: AppColors.kWhite,
+      body: BaseAuthenticationView(
+        canGoBack: true,
+        onMainActionButtonTapped: () {},
+        mainActionButtonText: 'Create new password',
+        hasSocialAuth: false,
+        form: Form(
+          child: Expanded(
+            child: Column(
+              children: [
+                CustomTextField(
+                  hintText: 'Password',
+                  textFieldColor: AppColors.kGrey50,
+                  keyBoardType: TextInputType.visiblePassword,
+                  obscureText: !viewState.passwordVisible,
+                  suffixIcon: CustomVisibilityButton(
+                    obscureText: viewState.passwordVisible,
+                    onTap: () {},
+                  ),
+                ),
+                Gap.md,
+                CustomTextField(
+                  hintText: 'Confirm password',
+                  textFieldColor: AppColors.kGrey50,
+                  keyBoardType: TextInputType.visiblePassword,
+                  obscureText: !viewState.confirmPasswordVisible,
+                  suffixIcon: CustomVisibilityButton(
+                    obscureText: viewState.confirmPasswordVisible,
+                    onTap: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        isLoading: false,
+        headerText: RichText(
+          text: TextSpan(
+            text: 'Create New Password',
+            style: AppTextStyles.kHeaderRegular.copyWith(
+              fontSize: FontSize.s24.sp,
+              color: AppColors.kSecondary,
+            ),
+            children: <TextSpan>[
+              const TextSpan(text: '\n\n'),
+              TextSpan(
+                text:
+                    'Please, enter a new password below different from the previous password',
+                style: AppTextStyles.kBodyRegular.copyWith(
+                  fontSize: FontSize.s16.sp,
+                  color: AppColors.kGrey500,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/ui/views/auth/reset_password/reset_password_viewmodel.dart
@@ -0,0 +1,65 @@
+import 'package:apex_network_take_home_project/src/core/navigation.dart';
+import 'package:apex_network_take_home_project/src/repository/auth/auth_impl.dart';
+import 'package:apex_network_take_home_project/src/services/api/failure.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/logger.dart';
+
+import '../../../core/enums/view_state.dart';
+
+final resetPasswordViewModel = StateNotifierProvider.autoDispose<
+    ResetPasswordViewModel, ResetPasswordViewState>(
+  (ref) => ResetPasswordViewModel(ref.read),
+);
+
+class ResetPasswordViewModel extends StateNotifier<ResetPasswordViewState> {
+  ResetPasswordViewModel(this._reader)
+      : super(ResetPasswordViewState.initial());
+
+  final Reader _reader;
+  final _log = Logger(filter: DevelopmentFilter());
+
+  void goToLoginView() {
+    _reader(navigationProvider).pop();
+  }
+
+  Future<void> resendCode() async {
+    state = state.copyWith(viewState: ViewState.loading);
+    try {
+      await _reader(authProvider)
+          .getEmailToken(email: _reader(authProvider).email!);
+    } on Failure catch (e) {
+      state = state.copyWith(viewState: ViewState.error);
+      _log.e(e);
+    } finally {
+      state = state.copyWith(viewState: ViewState.idle);
+    }
+  }
+}
+
+class ResetPasswordViewState {
+  final ViewState viewState;
+  final bool passwordVisible;
+  final bool confirmPasswordVisible;
+
+  const ResetPasswordViewState._(
+      {required this.viewState,
+      required this.passwordVisible,
+      required this.confirmPasswordVisible});
+
+  // initial state of the view
+  factory ResetPasswordViewState.initial() => const ResetPasswordViewState._(
+      viewState: ViewState.idle,
+      passwordVisible: false,
+      confirmPasswordVisible: false);
+
+  // using the default state 'idle' if no new state
+  ResetPasswordViewState copyWith(
+          {ViewState? viewState,
+          bool? passwordVisible,
+          bool? confirmPasswordVisible}) =>
+      ResetPasswordViewState._(
+          viewState: viewState ?? this.viewState,
+          passwordVisible: passwordVisible ?? this.passwordVisible,
+          confirmPasswordVisible:
+              confirmPasswordVisible ?? this.confirmPasswordVisible);
+}
