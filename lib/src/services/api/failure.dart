3c9f802b2bcb9b3eb61c1a452fd85e6360355f1f
Ademola <demolafadumo@gmail.com>
2022-06-01 11:06:41 +0100

    failure model updated

--- lib/src/services/api/failure.dart
+++ lib/src/services/api/failure.dart
@@ -1,20 +1,19 @@
 class Failure {
   final String message;
-  final Map<String, dynamic>? data;
+  final List<dynamic>? data;
+  final Map<String, dynamic>? errors;
 
-  Failure({
-    required this.message,
-    this.data,
-  });
+  Failure({required this.message, this.data, this.errors});
 
-  factory Failure.fromHttpErrorMap(Map<String, dynamic> json) => Failure(
-        message: json["error"]["message"],
+  factory Failure.fromJson(Map<String, dynamic> json) => Failure(
+        message: json["message"],
         data: json["data"],
+        errors: json["errors"],
       );
 
   @override
   String toString() {
-    return 'Failure{message: $message, data: $data}';
+    return 'Failure{message: $message, data: $data, errors: $errors}';
   }
 
   @override
@@ -23,8 +22,52 @@ class Failure {
       other is Failure &&
           runtimeType == other.runtimeType &&
           message == other.message &&
-          data == other.data;
+          data == other.data &&
+          errors == other.errors;
 
   @override
-  int get hashCode => message.hashCode ^ data.hashCode;
+  int get hashCode => message.hashCode ^ data.hashCode ^ errors.hashCode;
+}
+
+class AuthError {
+  bool? status;
+  String? message;
+  Errors? errors;
+
+  AuthError({this.status, this.message, this.errors});
+
+  AuthError.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+    errors = json['errors'] != null ? Errors.fromJson(json['errors']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['status'] = status;
+    data['message'] = message;
+    if (errors != null) {
+      data['errors'] = errors!.toJson();
+    }
+    return data;
+  }
+}
+
+class Errors {
+  List<String>? email;
+  List<String>? password;
+
+  Errors({this.email, this.password});
+
+  Errors.fromJson(Map<String, dynamic> json) {
+    email = json['email'].cast<String>();
+    password = json['password'].cast<String>();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['email'] = email;
+    data['password'] = password;
+    return data;
+  }
 }
