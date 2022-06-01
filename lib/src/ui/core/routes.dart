3e957a4b048a04dabb11c1c3356b4b30ccc57e15
Ademola <demolafadumo@gmail.com>
2022-06-01 11:09:38 +0100

    password recovery view, verify email view, reset password view, set pin code view, dashboard view added to routes

--- lib/src/ui/core/routes.dart
+++ lib/src/ui/core/routes.dart
@@ -1,10 +1,16 @@
 import 'dart:io';
 
 import 'package:apex_network_take_home_project/src/ui/views/auth/login/login_view.dart';
+import 'package:apex_network_take_home_project/src/ui/views/auth/password_recovery/password_recovery_view.dart';
+import 'package:apex_network_take_home_project/src/ui/views/auth/reset_password/reset_password_view.dart';
+import 'package:apex_network_take_home_project/src/ui/views/auth/set_pin_code/set_pin_code_view.dart';
 import 'package:apex_network_take_home_project/src/ui/views/auth/signup/signup_view.dart';
+import 'package:apex_network_take_home_project/src/ui/views/auth/verify_email/verify_email_view.dart';
 import 'package:apex_network_take_home_project/src/ui/views/main/onboarding/onboarding_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../views/home/dashboard/dashboard_view.dart';
 import '../views/startup/startup_view.dart';
 
 /// Routes
@@ -13,6 +19,11 @@ class Routes {
   static const onBoardingView = '/on_boarding_view';
   static const loginView = 'login_view';
   static const signupView = 'signup_view';
+  static const passwordRecoveryView = 'password_recovery_view';
+  static const verifyEmailView = 'verify_email_view';
+  static const resetPasswordView = 'reset_password_view';
+  static const setPinCodeView = 'set_pin_code_view';
+  static const dashboardView = 'dashboard_view';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     if (!Platform.isIOS) {
@@ -31,7 +42,27 @@ class Routes {
           );
         case signupView:
           return MaterialPageRoute(
-            builder: (_) => const SignupView(),
+            builder: (_) => SignupView(),
+          );
+        case verifyEmailView:
+          return MaterialPageRoute(
+            builder: (_) => VerifyEmailView(),
+          );
+        case passwordRecoveryView:
+          return MaterialPageRoute(
+            builder: (_) => const PasswordRecoveryView(),
+          );
+        case resetPasswordView:
+          return MaterialPageRoute(
+            builder: (_) => const ResetPasswordView(),
+          );
+        case setPinCodeView:
+          return MaterialPageRoute(
+            builder: (_) => SetPinCodeView(),
+          );
+        case dashboardView:
+          return MaterialPageRoute(
+            builder: (_) => const DashboardView(),
           );
         default:
           return MaterialPageRoute(
@@ -58,7 +89,27 @@ class Routes {
           );
         case signupView:
           return CupertinoPageRoute(
-            builder: (_) => const SignupView(),
+            builder: (_) => SignupView(),
+          );
+        case verifyEmailView:
+          return CupertinoPageRoute(
+            builder: (_) => VerifyEmailView(),
+          );
+        case passwordRecoveryView:
+          return CupertinoPageRoute(
+            builder: (_) => const PasswordRecoveryView(),
+          );
+        case resetPasswordView:
+          return CupertinoPageRoute(
+            builder: (_) => const ResetPasswordView(),
+          );
+        case setPinCodeView:
+          return CupertinoPageRoute(
+            builder: (_) => SetPinCodeView(),
+          );
+        case dashboardView:
+          return CupertinoPageRoute(
+            builder: (_) => const DashboardView(),
           );
         default:
           return CupertinoPageRoute(
