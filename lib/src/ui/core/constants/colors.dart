19ef61832701f778551f4c48f49444a4008927b6
Ademola <demolafadumo@gmail.com>
2022-06-02 02:46:16 +0100

    updated

--- lib/src/ui/core/constants/colors.dart
+++ lib/src/ui/core/constants/colors.dart
@@ -5,16 +5,15 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
-  // Standard colors
   static const kWhite = Color(0xFFFFFFFF);
   static const kBlack = Color(0xFF000000);
   static const kPrimary = Color(0xFFFFAB63);
   static const kSecondary = Color(0xFF111827);
 
-  // More colors
   static Color kGreen = Colors.green.shade600;
   static Color kRed = Colors.red.shade600;
 
+  static const kPrimary400 = Color(0xFFFFC08A);
   static const kGrey500 = Color(0xFF6B7280);
   static const kGrey400 = Color(0xFF9CA3AF);
   static const kGrey300 = Color(0xFFC4C4C4);
