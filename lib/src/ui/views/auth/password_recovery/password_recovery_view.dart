2e565dca1a47822ca07f2f49acfad477ae46e913
Ademola <demolafadumo@gmail.com>
2022-06-02 02:48:09 +0100

    password recovery view updated

--- lib/src/ui/views/auth/password_recovery/password_recovery_view.dart
+++ lib/src/ui/views/auth/password_recovery/password_recovery_view.dart
@@ -1,5 +1,8 @@
+import 'package:apex_network_take_home_project/src/ui/core/extensions/validation_extension.dart';
+import 'package:apex_network_take_home_project/src/ui/core/extensions/view_state.dart';
 import 'package:apex_network_take_home_project/src/ui/views/auth/password_recovery/password_recovery_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -10,27 +13,45 @@ import '../../../shared/stateless/text_field.dart';
 import '../base_auth/base_auth_view.dart';
 
 class PasswordRecoveryView extends HookConsumerWidget {
-  const PasswordRecoveryView({Key? key}) : super(key: key);
+  PasswordRecoveryView({Key? key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final viewState = ref.watch(passwordRecoveryViewModel);
+    final emailTextController = useTextEditingController();
     return Scaffold(
       backgroundColor: AppColors.kWhite,
       body: BaseAuthenticationView(
         canGoBack: true,
-        onMainActionButtonTapped: () {
-          ref.read(passwordRecoveryViewModel.notifier).goToResetPasswordView();
+        onMainActionButtonTapped: () async {
+          if (!_formKey.currentState!.validate()) {
+            return;
+          }
+          await ref
+              .read(passwordRecoveryViewModel.notifier)
+              .sendVerificationCode(email: emailTextController.text.trim())
+              .then((value) => ref
+                  .read(passwordRecoveryViewModel.notifier)
+                  .goToVerifyEmailTokenView()
+                  .then((value) => ref
+                      .read(passwordRecoveryViewModel.notifier)
+                      .goToResetPasswordView()));
         },
         mainActionButtonText: 'Send verification code',
         hasSocialAuth: false,
         form: Form(
+          key: _formKey,
           child: Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 CustomTextField(
                   hintText: 'Email',
+                  controller: emailTextController,
                   textFieldColor: AppColors.kGrey50,
+                  validator: context.validateEmail,
                   keyBoardType: TextInputType.emailAddress,
                   textStyle: AppTextStyles.kBodySemiBold.copyWith(
                     fontSize: FontSize.s16.sp,
@@ -40,7 +61,7 @@ class PasswordRecoveryView extends HookConsumerWidget {
             ),
           ),
         ),
-        isLoading: false,
+        isLoading: viewState.viewState.isLoading,
         headerText: RichText(
           text: TextSpan(
             text: 'Passsword Recovery',
--- lib/src/ui/views/auth/password_recovery/password_recovery_viewmodel.dart
+++ lib/src/ui/views/auth/password_recovery/password_recovery_viewmodel.dart
@@ -1,14 +1,15 @@
 import 'package:apex_network_take_home_project/src/core/navigation.dart';
 import 'package:apex_network_take_home_project/src/repository/auth/auth_impl.dart';
 import 'package:apex_network_take_home_project/src/services/api/failure.dart';
+import 'package:apex_network_take_home_project/src/services/snackbar_service.dart';
 import 'package:apex_network_take_home_project/src/ui/core/routes.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logger/logger.dart';
 
 import '../../../core/enums/view_state.dart';
 
-final passwordRecoveryViewModel = StateNotifierProvider.autoDispose<
-    PasswordRecoveryViewModel, PasswordRecoveryViewState>(
+final passwordRecoveryViewModel =
+    StateNotifierProvider<PasswordRecoveryViewModel, PasswordRecoveryViewState>(
   (ref) => PasswordRecoveryViewModel(ref.read),
 );
 
@@ -19,6 +20,25 @@ class PasswordRecoveryViewModel
 
   final Reader _reader;
   final _log = Logger(filter: DevelopmentFilter());
+
+  Future<void> sendVerificationCode({required String email}) async {
+    state = state.copyWith(viewState: ViewState.loading);
+    try {
+      String? token = await _reader(authRepository).getEmailToken(email: email);
+      _reader(authRepository).setEmail(email: email);
+      _reader(authRepository).setToken(token: token!);
+    } on Failure catch (e) {
+      state = state.copyWith(viewState: ViewState.error);
+      _reader(snackBarProvider).showErrorSnackBar(e.message);
+      _log.e(e);
+    } finally {
+      state = state.copyWith(viewState: ViewState.idle);
+    }
+  }
+
+  Future<void> goToVerifyEmailTokenView() async {
+    await _reader(navigationProvider).pushNamed(Routes.verifyEmailView);
+  }
 
   void goToResetPasswordView() {
     _reader(navigationProvider).pushNamed(Routes.resetPasswordView);
