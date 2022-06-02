27002b4d522f6064494696b3696cea1986563735
Ademola <demolafadumo@gmail.com>
2022-06-02 02:46:49 +0100

    dashboard respository functionality added

--- lib/src/repository/dashboard/dashboard.dart
+++ lib/src/repository/dashboard/dashboard.dart
@@ -1,1 +1,12 @@
-abstract class Dashboard {}
+abstract class Dashboard {
+  /// [Future]<[String]>? fetch dashboard secret.
+  ///
+  /// API Method: GET
+  ///
+  /// headers: {
+  ///   'Authorization': 'Bearer token'
+  /// }
+  ///
+  /// parameters: none
+  Future<String?> getDashboardSecret();
+}
--- lib/src/repository/dashboard/dashboard_impl.dart
+++ lib/src/repository/dashboard/dashboard_impl.dart
@@ -1,3 +1,25 @@
+import 'package:apex_network_take_home_project/src/core/constants/api_base.dart';
+import 'package:apex_network_take_home_project/src/model/dashboard/dashboard.dart';
+import 'package:apex_network_take_home_project/src/services/api/api.dart';
+import 'package:apex_network_take_home_project/src/services/api/api_service.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 import 'dashboard.dart';
 
-class DashboardImpl implements Dashboard {}
+final dashboardRepository = Provider(
+  (ref) => DashboardImpl(
+    ref.read(apiProvider),
+  ),
+);
+
+class DashboardImpl implements Dashboard {
+  DashboardImpl(this._api);
+
+  final Api _api;
+
+  @override
+  Future<String?> getDashboardSecret() async {
+    final response = await _api.get(ApiBase.dashboardUri(null));
+    return DashboardResponse.fromJson(response).data?.secret;
+  }
+}
